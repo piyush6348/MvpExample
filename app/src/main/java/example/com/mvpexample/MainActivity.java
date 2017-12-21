8f4d5f09dc847fa028cb7d6faa403581a17e8282
piyush6348 <piyush6348@gmail.com>
2017-12-22 00:08:52 +0530

    mvp completed

--- app/src/main/java/example/com/mvpexample/MainActivity.java
+++ app/src/main/java/example/com/mvpexample/MainActivity.java
@@ -3,11 +3,32 @@ package example.com.mvpexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import example.com.mvpexample.model.SignInPresentrImp;
+import example.com.mvpexample.view.SignInView;
 
+public class MainActivity extends AppCompatActivity implements SignInView{
+    private SignInPresentrImp signInPresentrImp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInPresentrImp = new SignInPresentrImp(MainActivity.this);
+        signInPresentrImp.signIn("piyush6348","123456");
+    }
+
+    @Override
+    public void showValidationError() {
+
+    }
+
+    @Override
+    public void signInSuccess() {
+
+    }
+
+    @Override
+    public void signInError() {
+
     }
 }

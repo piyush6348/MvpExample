a9d292c6e8a9d030dc632048b7ce1a5ad0c8c25e
piyush6348 <piyush6348@gmail.com>
2017-12-21 22:51:07 +0530

    view added

---
+++ app/src/main/java/example/com/mvpexample/view/SignInView.java
@@ -0,0 +1,11 @@
+package example.com.mvpexample.view;
+
+/**
+ * Created by piyush on 21/12/17.
+ */
+
+public interface SignInView {
+    void showValidationError();
+    void signInSuccess();
+    void signInError();
+}
